9a53146d0fc3553893f0e316d146576570756a9c
huskycat1202 <annie991202@naver.com>
2020-03-08 18:56:43 +0900

    20.03.08

--- js/button.js
+++ js/button.js
@@ -1,14 +1,14 @@
 var buton=0;
 function butclick(c){
     if(buton==c){
-        document.getEmentById('a'+c).style.height='0px';
-        buton=0; alert(c);
+        document.getElementById('a'+c).style.height='66px';
+        buton=0;
     }
     else{
         if(buton!=0){
-            document.getEmentById('a'+buton).style.height='0px';
+            document.getElementById('a'+buton).style.height='66px';
         }
-        document.getEmentById('a'+c).style.height='200px';
+        document.getElementById('a'+c).style.height='200px';
         buton=c;
     }
 }
